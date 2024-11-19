e188bea1e1ceeec733cef2c2949848f0772ab3f9
ObjectsCountries <kasramoayedi@protonmail.com>
2024-11-18 21:39:26 -0800

    Finish(?) LaTeX support

--- app/src/main/java/com/jherkenhoff/qalculate/ui/common/stringToLaTeX.kt
+++ app/src/main/java/com/jherkenhoff/qalculate/ui/common/stringToLaTeX.kt
@@ -2,35 +2,41 @@ package com.jherkenhoff.qalculate.ui.common
 
 fun stringToLaTeX(input: String): String {
     var formatted = input
-    formatted = Regex("<sup>(.*)</sup>").replace(formatted) { match ->
-        "^{"+match.groupValues[1]+"}"
-    }
-    formatted = Regex("<sub>(.*)</sub>").replace(formatted) { match ->
-        "_{"+match.groupValues[1]+"}"
-    }
-    formatted = Regex("<i>'?(.+?)'?</i>").replace(formatted) { match ->
-        match.groupValues[1]
-    }
-    formatted = Regex("<span style=\"color:#(005858|585800)\">(.+?)</span>").replace(formatted) { match ->
+    formatted = Regex("(&nbsp;)?<i>'?(.+?)'?</i>").replace(formatted) { match ->
         match.groupValues[2]
     }
-    formatted = Regex("<span style=\"color:#008000\">((.+?) ∕ (.+?))+?</span>").replace(formatted) { match ->
+    formatted = Regex("(&nbsp;)?<span style=\"color:#005858\">(.+?)</span>").replace(formatted) { match ->
+        match.groupValues[2]
+    }
+    formatted = Regex("(&nbsp;)?<span style=\"color:#585800\">(.+?)</span>").replace(formatted) { match ->
+        match.groupValues[2]
+    }
+    formatted = Regex("(&nbsp;)?<sup>(.+?)</sup>").replace(formatted) { match ->
+        "^{"+match.groupValues[2]+"}"
+    }
+    formatted = Regex("(&nbsp;)?<sub>(.+?)</sub>").replace(formatted) { match ->
+        "_{"+match.groupValues[2]+"}"
+    }
+    formatted = Regex("(&nbsp;)?<span style=\"color:#008000\">([^<]+?) ∕ ([^<]+?)</span>").replace(formatted) { match ->
         "\\frac{\\text{"+match.groupValues[2]+"}}{\\text{"+match.groupValues[3]+"}}"
     }
-    formatted = Regex("<span style=\"color:#008000\">(.+?)</span>").replace(formatted) { match ->
-        "\\text{"+match.groupValues[1]+"}"
-    }
-    formatted = Regex("\\(((.+?) ∕ (.+?))+?\\)").replace(formatted) { match ->
-        "\\left(\\frac{"+match.groupValues[2]+"}{"+match.groupValues[3]+"}\\right)"
-    }
-    formatted = Regex("((.+?) ∕ (.+?))+?").replace(formatted) { match ->
+    formatted = Regex("(&nbsp;)?(.+?) ∕ (.+?)").replace(formatted) { match ->
         "\\frac{"+match.groupValues[2]+"}{"+match.groupValues[3]+"}"
     }
-    // TODO
-    formatted = Regex("TODO(.*?)").replace(formatted) { match ->
-        "j"
+    formatted = Regex("(&nbsp;)?<span style=\"color:#008000\">(.+?)</span>").replace(formatted) { match ->
+        "\\text{"+match.groupValues[2]+"}"
     }
-    formatted = formatted.replace("&nbsp;", "")
+    formatted = formatted.replace("[", "\\left\\langle ") // space at the end is necessary so that a variable as the first element of the vector doesn't get interpreted as part of the word "langle"
+    formatted = formatted.replace("]", "\\right\\rangle ")
+    if (formatted.contains('[') && formatted.contains(']')) {
+        formatted = formatted.replace("&nbsp;", ",")
+    }
+    else {
+        formatted = formatted.replace("&nbsp;", "")
+    }
+    formatted = Regex("(cos|csc|exp|ker|limsup|min|sinh|arcsin|cosh|deg|gcd|lg|ln|Pr|sup|arctan|cot|det|hom|lim|log|sec|tan|arg|coth|dim|liminf|max|sin|tanh|int|iint|iiint|iiiint|idotsint|oint|sum|prod|alpha|beta|gamma|Gamma|delta|Delta|epsilon|varepsilon|zeta|eta|theta|vartheta|Theta|iota|kappa|lambda|Lambda|mu|nu|xi|Xi|pi|Pi|rho|varrho|sigma|Sigma|tau|upsilon|Upsilon|phi|varphi|Phi|chi|psi|Psi|omega|Omega)").replace(formatted) { match ->
+        "\\"+match.groupValues[1]
+    }
     formatted = formatted.replace("&lt;", "<")
     formatted = formatted.replace("≤", "\\le")
     formatted = formatted.replace("&gt;", ">")
